76bf86f7b38e748914150f0dfa41a8979b57cc83
Johnny Choi <cchoi14@ucsc.edu>
2021-05-05 03:53:40 +0900

    pigion dives left to right

--- src/Prefabs/Pigeon.js
+++ src/Prefabs/Pigeon.js
@@ -3,22 +3,17 @@ class Pigeon extends Phaser.GameObjects.Sprite {
         super(scene, x, y, texture, frame);
         scene.add.existing(this);   //adds to existing scene
         //this.points = 1;   //stores pointValue
-        this.moveSpeed = 3;         //pixels per frame birds move at
+        this.moveSpeed = 5;         //pixels per frame birds move at
     }
 
     update() {
-        //birds move right
+        //birds move left
         this.x += this.moveSpeed;
 
         //if bird hits end of screen, reset to other side
-        if(this.x <= 0 - this.width) {
-            this.x = game.config.width;
+        if(this.x >= game.config.width + this.width) {
+            this.x = 0 - 50;
         }
-    }
-
-    reset() {
-        this.x = game.config.width + 50;
-        this.alpha = 1;
     }
 
     moveSpeedFast() {
