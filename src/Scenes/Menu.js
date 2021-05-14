1a32ef3af641a47bf07eb23f7a506b28adbe5e10
Nanxiang Wang <nwang22@ucsc.edu>
2021-05-14 15:58:48 -0700

    Added more instructions

--- src/Scenes/Menu.js
+++ src/Scenes/Menu.js
@@ -6,30 +6,13 @@ class Menu extends Phaser.Scene {
     preload() {
         // load audio
         this.load.audio('sfx_select','./Assets/sounds/endlessRunner_Select.wav');
-
+        
+        //load image
+        this.load.image('instructions', './Assets/backgrounds/instructions.png');
     }
     create() { 
-        this.background = this.add.tileSprite(0, 0, 1280, 720, 'background_title').setOrigin(0, 0);
+        this.background = this.add.tileSprite(0, 0, 1280, 720, 'instructions').setOrigin(0, 0);
 
-        let menuConfig = {
-            fontFamily: 'Bread and Circus',
-            fontSize: '28px',
-            backgroundColor: '#F3B141',
-            color: '#843605',
-            align: 'right',
-            padding: {
-                top: 5,
-                bottom: 5,
-                left: 5,
-                right: 5,
-            },
-            fixedWidth: 0
-        }
-        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'Welcome to Bread Runner!', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2, 'Run and jump on traffic as you try to avoid having your bread being stolen by pesky birds.', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Instructions: SPACE to jump and ← and → to move.', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2 + (borderUISize + borderPadding) * 2, 'Press SPACE to start.', menuConfig).setOrigin(0.5);
-	
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     	
     }
